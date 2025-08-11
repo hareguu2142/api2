cfadf4eab8967387a0e4f8874a24428a368f9c41
kim <isomorphism2016@gmail.com>
2025-08-11 23:27:27 +0900

    d

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
 from fastapi import FastAPI
+from typing import List
 
 app = FastAPI(title="api2")
+
+def get_prime_factors(n: int) -> List[int]:
+    factors = []
+    d = 2
+    while d * d <= n:
+        while (n % d) == 0:
+            factors.append(d)
+            n //= d
+        d += 1
+    if n > 1:
+       factors.append(n)
+    return factors
 
 @app.get("/")
 def root():
@@ -13,3 +26,10 @@ def ping():
 @app.get("/healthz")
 def health():
     return {"ok": True}
+
+@app.get("/factorize/{number}")
+def factorize(number: int):
+    """
+    Performs prime factorization on the given number.
+    """
+    return {"number": number, "factors": get_prime_factors(number)}
